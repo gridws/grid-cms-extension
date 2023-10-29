ccdb5e6df52c98b1300e3e86044c0e50b15c2855
Stefan <stefan@oky.dk>
2023-10-29 18:34:39 +0100

    Manifest setup

--- functions/index.js
+++ functions/index.js
@@ -39,28 +39,32 @@ export const server = onRequest(async (req, res) => {
 		]
 	}
 
-	res.send(`<!DOCTYPE html>
+	if (req.path.startsWith('/manifest.json')) {
+		res.json(manifest)
+	} else {
+		res.send(`<!DOCTYPE html>
     <html>
-      <head>
-        <meta charset="utf-8" />
-        <title>${NAME}</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#fff" />
-        <link rel="manifest" href='data:application/manifest+json,${encodeURIComponent(JSON.stringify(manifest))}' />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="color-scheme" content="dark light" />
-      </head>
-      <body class="light">
-        <div id="cms"></div>
-        <noscript>Enable JS to continue.</noscript>
-        <script type="importmap">{"imports": { "grid-cms": "https://esm.run/grid-cms@1"}}</script>
-        <script type="module">
-          import { setup } from 'grid-cms'
-          setup({
-            config: ${JSON.stringify(config)},
-            firebaseConfig: ${JSON.stringify(firebaseConfig)}
-          })          
-        </script>
-      </body>
+		<head>
+		<meta charset="utf-8" />
+		<title>${NAME}</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name="theme-color" content="#fff" />
+		<link rel="manifest" href="/manifest.json" />
+		<meta name="apple-mobile-web-app-capable" content="yes" />
+		<meta name="color-scheme" content="dark light" />
+		</head>
+		<body class="light">
+		<div id="cms"></div>
+		<noscript>Enable JS to continue.</noscript>
+		<script type="importmap">{"imports": { "grid-cms": "https://esm.run/grid-cms@1"}}</script>
+		<script type="module">
+		import { setup } from 'grid-cms'
+		setup({
+			config: ${JSON.stringify(config)},
+			firebaseConfig: ${JSON.stringify(firebaseConfig)}
+		})          
+		</script>
+		</body>
     </html>`)
+	}
 })
